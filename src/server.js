39fdad5547c98be10c52803f50a14e8617f34774
zadhart <wams@ic.ufal.br>
2021-06-23 18:26:55 -0300

    Managing errors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("express-async-errors");
 var express_1 = __importDefault(require("express"));
 require("reflect-metadata");
 var routes_1 = require("./routes");
@@ -10,4 +11,15 @@ require("./database");
 var app = express_1.default();
 app.use(express_1.default.json());
 app.use(routes_1.router);
+app.use(function (err, request, response, next) {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
 app.listen(3000, function () { return console.log("Server is running..."); });
