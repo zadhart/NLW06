39a81ee3883b672e10063b7c62e70bae3a2f6612
zadhart <wams@ic.ufal.br>
2021-06-23 17:18:41 -0300

    Fixed some bugs

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 require("reflect-metadata");
+var routes_1 = require("./routes");
 require("./database");
 var app = express_1.default();
+app.use(express_1.default.json());
+app.use(routes_1.router);
 app.listen(3000, function () { return console.log("Server is running..."); });
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import "reflect-metadata";
+import { router } from "./routes";
 
 import "./database"
 
 const app = express();
 
-app.listen(3000, () => console.log("Server is running..."))
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3000, () => console.log("Server is running..."));
